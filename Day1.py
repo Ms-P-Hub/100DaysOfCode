190d97d9ea93ef3f1890ae2c260d36e8861ba40e
Gugu Precious Mokwena <gugu.codes@gmail.com>
2023-02-11 15:37:21 +0200

    Band Name Generator

---
+++ Day1.py
@@ -0,0 +1,6 @@
+"""BANDNAME GENERATOR PROGRAM"""
+
+print("Welcome to the Band Name Generator.")
+city = input("What is the name of the city you were born in?\n")
+pet_name = input("What is your pet's name?\n")
+print(f"Your Band name could be {city.capitalize()} {pet_name.capitalize()}")
