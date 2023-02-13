c6a0c855238a454ad64cbcd2f4b3eddd786abcee
Gugu Precious Mokwena <gugu.codes@gmail.com>
2023-02-13 22:46:35 +0200

    FizzBuzz Challenge

---
+++ Fizzbuzz.py
@@ -0,0 +1,11 @@
+stop = int(input("Please enter a number you would like to stop on: "))
+
+for i in range(1,(stop+1)):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
