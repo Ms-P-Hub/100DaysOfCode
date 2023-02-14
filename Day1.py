a01ae7cfc8c154c3a00bbb97ca3d21b1469439aa
Gugu Precious Mokwena <gugu.codes@gmail.com>
2023-02-14 21:45:31 +0200

    Added Validations

--- Day1.py
+++ Day1.py
@@ -2,5 +2,13 @@
 
 print("Welcome to the Band Name Generator.")
 city = input("What is the name of the city you were born in?\n")
+while city.isalpha() == False:
+    print("Error please enter a string")
+    city = input("What is the name of the city you were born in?\n")
+
 pet_name = input("What is your pet's name?\n")
-print(f"Your Band name could be {city.capitalize()} {pet_name.capitalize()}")
+while pet_name.isalpha() == False:
+    print("Error please enter a string")
+    pet_name = input("What is your pet's name?\n")
+    
+print(f"Your Band name could be {city.capitalize()}-{pet_name.capitalize()}")
